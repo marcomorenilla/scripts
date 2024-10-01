8a09514822b8a1cab1e1e866695d980df215ee7d
Marco Antonio Morenilla Alonso <mmorealo@myuax.com>
2024-10-01 22:44:16 +0200

    removing duplicated file

--- gitAdd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p 'enter a message for this commit ' message
-read -p 'enter the name of the branch that you want to push to github ' destination
-git add -Av
-git commit -m "${message}"
-git push github $destination
